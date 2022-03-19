f25730ec076019628d4bdc7fe8d960b000a38d7e
Kev9794 <k.natanael88@gmail.com>
2022-03-19 22:26:04 +0700

    Beberapa Command Menampilkan Record

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -13,7 +13,29 @@ class PegawaiController extends Controller
     public function index()
     {
     	// mengambil data pegawai
-    	$pegawai = Pegawai::all();
+        // $pegawai = Pegawai::all();
+        
+        // mengambil data pegawai yang pertama
+        // $pegawai = Pegawai::first();
+
+        // mengambil data pegawai yang id nya 1
+        // $pegawai = Pegawai::find(1);
+
+        // mengambil data pegawai yang bernama Jamal Uwais
+        // $pegawai = Pegawai::where('nama', 'Siska Aryani')->get();
+        // $pegawai = Pegawai::where('nama', '=', 'Jamal Uwais')->get();
+
+        // mengambil data pegawai yang id nya lebih besar dari 10
+        // $pegawai = Pegawai::where('id', '>', 10)->get();
+
+        // mengambil data pegawai yang id nya lebih besar sama dengan 10 
+        // $pegawai = Pegawai::where('id', '>=', 10)->get();
+
+        // mengambil data pegawai yang di namanya ada huruf a 
+        // $pegawai = Pegawai::where('nama', 'like', '%a%')->get();
+
+        // menampilkan 10 data pegawai per halaman
+        $pegawai = Pegawai::paginate(10);
 
     	// mengirim data pegawai ke view pegawai
     	return view('pegawai', ['pegawai' => $pegawai]);
